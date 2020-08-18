30b08932ebe0d3a2e06263a17e312b6997b3745d
unknown <lequynhkma@gmail.com>
2020-08-18 20:02:57 +0700

    chuc nang Done + edit note

--- app/src/main/java/com/tth/test/ui/personal/NoteAdapter.java
+++ app/src/main/java/com/tth/test/ui/personal/NoteAdapter.java
@@ -5,14 +5,21 @@ import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.PopupMenu;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,17 +29,23 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.tth.test.R;
 import com.tth.test.db.DBHelper;
 import com.tth.test.model.Note;
+import com.tth.test.model.Work;
+import com.tth.test.util.KeyboardUtils;
 
 import org.w3c.dom.Text;
 
 import java.util.List;
 import java.util.zip.Inflater;
 
+import static com.google.android.material.datepicker.MaterialDatePicker.Builder.datePicker;
+import static java.security.AccessController.getContext;
+
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
     private List<Note> note;
     private Context context;
     DBHelper dbHelper;
     NoteFragment noteFragment ;
+    String time = "";
     private ArrayAdapter<Note> listViewAdapter;
 //    private ItemClickListener mClickListener;
     public NoteAdapter(Context context, List<Note> note){
@@ -55,15 +68,21 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
         final Note notes = note.get(position);
         holder.noteContent.setText(notes.getContent());
         holder.noteTitle.setText(notes.getTitle());
-        if(notes.getChecked()==1) {
-            holder.itemView.setBackgroundColor(Color.parseColor("#ffffff"));
+        if (notes.getChecked() == 1) {
+            holder.itemView.setBackgroundColor(Color.parseColor("#dce3dc"));
         }
         holder.itemView.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "Click Long"+notes.getTitle(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "Click Long" + notes.getTitle(), Toast.LENGTH_SHORT).show();
 
+            }
+        });
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopUpWindowEdit(view, position);
             }
         });
 
@@ -78,19 +97,24 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
                     @Override
                     public boolean onMenuItemClick(MenuItem menuItem) {
                         switch (menuItem.getItemId()) {
-                            case R.id.itemMenu_edit:
-                                Toast.makeText(context, notes.getChecked(), Toast.LENGTH_LONG);
-                                break;
                             case R.id.itemMenu_done:
                                 dbHelper = new DBHelper(context);
-                                notes.setChecked(1);
-                                notifyDataSetChanged();
+                                if(notes.getChecked()==0) {
+                                    notes.setChecked(1);
+                                    dbHelper.updateNote(notes);
+                                    notifyDataSetChanged();
+                                }
+                                else
+                                {
+                                    notes.setChecked(0);
+                                    dbHelper.updateNote(notes);
+                                    notifyDataSetChanged();
+                                }
                                 break;
                             case R.id.itemMenu_delete:
                                 dbHelper = new DBHelper(context);
                                 dbHelper.deleteNote(notes);
                                 note.remove(notes);
-                                Toast.makeText(context, "deleted", Toast.LENGTH_LONG);
                                 //refresh
                                 notifyDataSetChanged();
                                 break;
@@ -126,17 +150,95 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
         }
 
     }
-//        @Override
-//        public void onClick(View view) {
-//            if (mClickListener !=null) mClickListener.onItemClick(view, getAdapterPosition());
-//        }
-//    }
-//        String getItem(int id){
-//        return note[id];
-//    }
-//        void setClickListener(ItemClickListener itemClickListener){
-//            this.mClickListener = itemClickListener;
-//        }
-//    public interface ItemClickListener{
-//        void onItemClick(View view, int position);
-}
+    public void showPopUpWindowEdit(final View view, final int position) {
+        LayoutInflater layoutInflater = (LayoutInflater) view.getContext().getSystemService(view.getContext().LAYOUT_INFLATER_SERVICE);
+        final View popupView = layoutInflater.inflate(R.layout.activity_add_note, null);
+        int width = LinearLayout.LayoutParams.MATCH_PARENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true;
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 70);
+        final TextView title = popupView.findViewById(R.id.edt_titleNote);
+        final TextView content =  popupView.findViewById(R.id.edt_note);
+        //get work
+        final Note no = note.get(position);
+        title.setText(no.getTitle());
+        content.setText(no.getContent());
+        final String last = no.getLast_mdf();
+        //LAM MO NEN
+        View container = popupWindow.getContentView().getRootView();
+        if (container != null) {
+            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            WindowManager.LayoutParams p = (WindowManager.LayoutParams) container.getLayoutParams();
+            p.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+            p.dimAmount = 0.3f;
+            if (wm != null) {
+                wm.updateViewLayout(container, p);
+            }
+        }
+        final Button button = popupView.findViewById(R.id.button_time);
+        final Button button2 = popupView.findViewById(R.id.button_done);
+        if (last.equals("") == true) {
+            button.setText("Đặt nhắc nhở");
+        } else {
+            button.setText(last);
+        }
+        button2.setText("Hoàn tất");
+        button2.setTextColor(Color.GREEN);
+        title.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (last.equals("") == false) {
+                    button.setText(last);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.toString().trim().length() > 0) {
+                    button2.setTextColor(Color.GREEN);
+                    button2.setEnabled(true);
+                } else {
+                    button2.setTextColor(Color.GRAY);
+                    button2.setEnabled(false);
+                }
+            }
+        });
+        //set time
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePicker();
+            }
+        });
+        //save
+        if (content.getText() == "") button2.setEnabled(false);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                no.setTitle(title.getText().toString());
+                no.setContent(content.getText().toString());
+                if (time.equals("") == false) {
+                    no.setLast_mdf(time);
+                }
+                dbHelper = new DBHelper(context);
+
+                dbHelper.updateNote(no);
+                notifyItemChanged(position);
+                time = "";
+                popupWindow.dismiss();
+            }
+        });
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                KeyboardUtils.showKeyboard2(view);
+            }
+        });
+        KeyboardUtils.showKeyboard2(title);
+            }
+    }
--- app/src/main/java/com/tth/test/ui/personal/NoteFragment.java
+++ app/src/main/java/com/tth/test/ui/personal/NoteFragment.java
@@ -1,17 +1,21 @@
 package com.tth.test.ui.personal;
 import androidx.fragment.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -29,6 +33,8 @@ import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.tth.test.model.Work;
+import com.tth.test.ui.personal.SecureFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.tth.test.R;
 import com.tth.test.db.DBHelper;
@@ -38,6 +44,8 @@ import com.tth.test.util.KeyboardUtils;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+
+import static com.google.android.material.datepicker.MaterialDatePicker.Builder.datePicker;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -94,7 +102,7 @@ public class NoteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_note,container,false);
+        View view = inflater.inflate(R.layout.fragment_note, container, false);
         //kb note
         note = new ArrayList<>();
         dbHelper = new DBHelper(getActivity());
@@ -105,17 +113,121 @@ public class NoteFragment extends Fragment {
         recyclerView.setHasFixedSize(true);
         // add btn
         FloatingActionButton add_button = view.findViewById(R.id.add_button);
+        FloatingActionButton secure_button = view.findViewById(R.id.secure_button);
         empty_imageview = view.findViewById(R.id.empty_imageview);
         no_data = view.findViewById(R.id.no_data);
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 AddNote addNote = new AddNote();
                 addNote.showPopUpWindow(view);
+            }
+        });
+        secure_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("He", "click secure");
+
             }
         });
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(numberOfColumns, StaggeredGridLayoutManager.VERTICAL));
         recyclerView.setAdapter(new NoteAdapter(getActivity(), note));
         return view;
     }
-}
+        //click
+        //POPUP ADD WORK
+//        public void showPopUpWindow(final View view) {
+//            LayoutInflater layoutInflater = (LayoutInflater) view.getContext().getSystemService(view.getContext().LAYOUT_INFLATER_SERVICE);
+//            final View popupView = layoutInflater.inflate(R.layout.activity_add_note, null);
+//            int width = LinearLayout.LayoutParams.MATCH_PARENT;
+//            int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+//            boolean focusable = true;
+//            final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+//            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 85);
+//            //LAM MO NEN
+//            View container = popupWindow.getContentView().getRootView();
+//            if (container != null) {
+//                WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+//                WindowManager.LayoutParams p = (WindowManager.LayoutParams) container.getLayoutParams();
+//                p.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+//                p.dimAmount = 0.3f;
+//                if (wm != null) {
+//                    wm.updateViewLayout(container, p);
+//                }
+//            }
+//
+//            final TextView title = popupView.findViewById(R.id.tv_title);
+//            final TextView content = popupView.findViewById(R.id.tv_content);
+//            final Button button = popupView.findViewById(R.id.button_time);
+//            final Button button2 = popupView.findViewById(R.id.button_done);
+//            button.setText("Đặt nhắc nhở");
+//            button2.setText("Hoàn tất");
+//            title.addTextChangedListener(new TextWatcher() {
+//                @Override
+//                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                }
+//
+//                @Override
+//                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+////                    if (time.equals("") == false) {
+////                        button.setText(time);
+////                    }
+//                }
+//
+//                @Override
+//                public void afterTextChanged(Editable editable) {
+//                    if (editable.toString().trim().length() > 0) {
+//                        button2.setTextColor(Color.GREEN);
+//                        button2.setEnabled(true);
+//                    } else {
+//                        button2.setTextColor(Color.GRAY);
+//                        button2.setEnabled(false);
+//                    }
+//                }
+//            });
+//            //set time
+//            button.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    datePicker();
+//                }
+//            });
+//            //save
+//            button2.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    DBHelper dbHelper = new DBHelper(view.getContext());
+//                    String noteContent = content.getText().toString();
+//                    String noteTitle = title.getText().toString();
+//                    Note no = new Note(noteTitle,noteContent , "anc",0);
+//                    //addwork(wo);
+//                    dbHelper.addNote(no);
+//
+//                    //time="";
+//                    popupWindow.dismiss();
+//                }
+//            });
+//            //nhan vao khu vuc cua popup
+//            popupView.setOnTouchListener(new View.OnTouchListener() {
+//                @Override
+//                public boolean onTouch(View view, MotionEvent motionEvent) {
+//                    //popupWindow.dismiss();
+//                    return true;
+//                }
+//            });
+//            //click out popup
+//            popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+//                @Override
+//                public void onDismiss() {
+//                    KeyboardUtils.showKeyboard2(view);
+//                }
+//            });
+//            KeyboardUtils.showKeyboard2(title);
+//        }
+
+    public void addNote(Note note){
+       dbHelper.addNote(note);
+    }
+
+    }
